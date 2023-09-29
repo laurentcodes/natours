679a7a5d00a1311c1201363a6e3ec651002dd2b2
Saint Laurent <stlaurentcodes@gmail.com>
2023-09-29 15:39:15 +0100

    feat: complete tour flow, error handling and setup user model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       minLength: [10, 'A tour name must have more or equal than 10 characters'],
       maxLength: [40, 'A tour name must have less or equal than 40 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -44,8 +46,12 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
-      validate: function (val) {
-        return val < this.price; // 100 < 200
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price; // 100 < 200
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
       },
     },
     summary: {
