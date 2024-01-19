30d5999d8f70a875ef8685da7756d2d454f842f5
Saint Laurent <stlaurentcodes@gmail.com>
2024-01-19 12:53:09 +0100

    fix: fix mail and proxy

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const bookingController = require('./controllers/bookingController');
 
 const app = express();
 
-app.enable('trust proxy');
+// app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
